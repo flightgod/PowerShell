1a20c6ee009c6837eb75a6351d4db042d6a4f403
Kevin Bennett <flightgod@gmail.com>
2019-05-28 09:12:44 -0500

    Cleanup

--- Check-Website_Status.ps1
+++ Check-Website_Status.ps1
@@ -11,32 +11,3 @@ If ($statusCode -eq 200) {
 } Else { 
     Write-Host "Site is down"
 }
-
-# Install the PowerShell Module
-#Install-Module -Name Microsoft.Online.SharePoint.PowerShell
-
-#Import and Connect
-Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
-
-Connect-SPOService -Url https://test.sharepoint.com
-
-
-# Function to Update SharePoint List
-#Need to make o365 Connection
-Function UpdateStatus {
-    $SPWeb = Get-SPWeb https://test.sharepoint.com/sites/EnterpriseSystems2
-    $List = $SPWeb.Lists["Environment Status"]
-    $items = $List.Items
-<#    foreach ($item in $items) {
-        $taskStatus = $item["Status"]
-        $docStatus = $item["Document Status"]
-        if ($taskStatus -eq "Completed" -and $docStatus -eq "Pending") {
-            $item["Document Status"] = "Approved"
-            $item.Update()
-            $list.Update()
-        }
-} #>
-$SPWeb.Dispose()
-
-}
-
